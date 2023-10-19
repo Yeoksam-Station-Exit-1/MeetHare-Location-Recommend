387447bae66877d6a0b961e072cf96a6668e3f4f
chaeyeong222 <iky1505@naver.com>
2023-10-19 17:17:06 +0900

    :wrench: 기본환경 세팅

---
+++ locationRecommend/src/main/java/YeoksamStationExit1/locationRecommend/controller/LocationController.java
@@ -0,0 +1,13 @@
+package YeoksamStationExit1.locationRecommend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/map")
+@Slf4j
+public class LocationController {
+}
---
+++ locationRecommend/src/test/java/YeoksamStationExit1/locationRecommend/LocationRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package YeoksamStationExit1.locationRecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
